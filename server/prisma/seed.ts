feef2f080c6836dd242f18d094a19f6249f9ffd0
adzanilrachmadhip <adzanilrachmadip@gmail.com>
2025-12-15 12:34:35 +0700

    add user seeder

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,9 +1,14 @@
 import { PrismaClient } from "@prisma/client";
+import { hashSync } from "bcryptjs";
 
 const prisma = new PrismaClient();
 
 async function main() {
-  // Create dummy user
+
+  // passwords 
+  const hashedPassword = hashSync("1234567", 10);
+
+  // user
   const user = await prisma.user.upsert({
     where: { email: "demo@skillbridge.com" },
     update: {},
@@ -11,11 +16,27 @@ async function main() {
       id: "dummy-user-123",
       email: "demo@skillbridge.com",
       name: "Demo User",
+      password: hashedPassword,
+      provider: "email",
+      isVerified: true,
     },
   });
-
-  console.log("Created user:", user);
+  console.log("Berhasil Membuat user:", user);
 }
+
+// opsional
+// const adminUser = await prisma.user.upsert({
+//   where: {email: "admin@skillbridge.com"},
+//   update: {},
+//   create: {
+//     email: "",
+//     name: "Admin User",
+//     password: hashSync("admin123", 10),
+//     role: "ADMIN",
+//     provider: "email",
+//     isVerified: true,
+//   },
+// });
 
 main()
   .catch((e) => {
